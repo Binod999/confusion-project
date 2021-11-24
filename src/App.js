7d2677af2b34c6dca7a82a5a8497c523817e7bc4
Binod Patel <binod.patel@capgemini.com>
2021-11-24 12:43:33 +0530

    Assignment 2 Changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
 import './App.css';
-import { Navbar,NavbarBrand } from 'reactstrap';
-import React, {Component} from 'react';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
-
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
   render(){
-    return (
-      <div>
-          <Navbar dark color ="primary">
-            <div className="container">
-              <NavbarBrand href="/">
-                 Ristorante Confusion
-              </NavbarBrand>
-            </div>
-          </Navbar>
-          <Menu dishes={this.state.dishes} />
-      </div>
-    )
+    return(
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>      
+      </BrowserRouter>
+    );
   }
 }
 export default App;
